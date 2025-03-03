14e6f95b1ce0aa93738f6371db3d1e79184d1b81
keiko233 <i@elaina.moe>
2025-03-03 18:50:41 +0800

    chore: init

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import mdplugin from "@libnyanpasu/material-design-tailwind";
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: "class",
+  content: [
+    "./node_modules/@libnyanpasu/material-design-react/**/*.js",
+    "./node_modules/@libnyanpasu/material-design-components/**/*.js",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  plugins: [mdplugin],
+} satisfies Config;
