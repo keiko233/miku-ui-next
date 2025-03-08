0be2c78b36e1482b5344b91d33c3e4ae094aacc6
keiko233 <i@elaina.moe>
2025-03-08 20:17:04 +0000

    feat: add unplugin-icons support and enhance dark mode toggle functionality

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,24 @@
 import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
- 
-initOpenNextCloudflareForDev();
-
 import type { NextConfig } from "next";
+import unpluginIcons from "unplugin-icons/webpack";
+
+initOpenNextCloudflareForDev();
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   experimental: {
     typedRoutes: true,
+  },
+
+  webpack(config) {
+    config.plugins.push(
+      unpluginIcons({
+        compiler: "jsx",
+        jsx: "react",
+      }),
+    );
+
+    return config;
   },
 };
 
--- src/components/dark-mode.tsx
+++ src/components/dark-mode.tsx
@@ -1,14 +1,20 @@
 "use client";
 
 import { Button } from "@libnyanpasu/material-design-react";
+import MaterialSymbolsDarkModeOutlineRounded from "~icons/material-symbols/dark-mode-outline-rounded";
+import MaterialSymbolsLightModeOutlineRounded from "~icons/material-symbols/light-mode-outline-rounded";
 import { useThemeContext } from "./providers/theme-provider";
 
 export const DrakMode = () => {
-  const { toggle } = useThemeContext();
+  const { dark, toggle } = useThemeContext();
 
   return (
     <Button icon variant="flat" onClick={toggle}>
-      T
+      {dark ? (
+        <MaterialSymbolsLightModeOutlineRounded />
+      ) : (
+        <MaterialSymbolsDarkModeOutlineRounded />
+      )}
     </Button>
   );
 };
