fa6a232b27511408283d531b8304ff3ac05788fd
keiko233 <i@elaina.moe>
2025-03-08 21:34:31 +0000

    refactor(schema): use zod validation schemas

--- src/schema.ts
+++ src/schema.ts
@@ -1,12 +1,18 @@
+import { z } from "zod";
+
 export enum DeviceStatus {
   COMMUNITY = "COMMUNITY",
   OFFICIAL = "OFFICIAL",
 }
 
+export const DeviceStatusEnum = z.nativeEnum(DeviceStatus);
+
 export enum SELinuxStatus {
   Enforcing = "Enforcing",
   Permissive = "Permissive",
 }
+
+export const SELinuxStatusEnum = z.nativeEnum(SELinuxStatus);
 
 export enum TaskStatus {
   TODO = "TODO",
@@ -15,40 +21,49 @@ export enum TaskStatus {
   FAILED = "FAILED",
 }
 
-export interface Device {
-  id: string;
-  codename: string;
-  name: string;
-  version: string;
-  androidVersion: number;
-  status: DeviceStatus;
-  selinuxStatus: SELinuxStatus;
-  kernelsuVersion: number;
-  sourcforgeUrl: string;
-  changelog: string | null;
-  note: string | null;
-  publishAt: number;
-  createdAt: number;
-  updatedAt: number;
-}
+export const TaskStatusEnum = z.nativeEnum(TaskStatus);
 
-export interface Task {
-  id: string;
-  title: string;
-  content: string;
-  status: TaskStatus;
-  createdAt: number;
-  updatedAt: number;
-}
+export const DeviceSchema = z.object({
+  id: z.string(),
+  codename: z.string(),
+  name: z.string(),
+  version: z.string(),
+  androidVersion: z.number(),
+  status: DeviceStatusEnum,
+  selinuxStatus: SELinuxStatusEnum,
+  kernelsuVersion: z.number(),
+  sourcforgeUrl: z.string(),
+  changelog: z.string().nullable(),
+  note: z.string().nullable(),
+  publishAt: z.number(),
+  createdAt: z.number(),
+  updatedAt: z.number(),
+});
 
-export interface Context {
-  id: string;
-  index: number;
-  content: string;
-  createdAt: number;
-  updatedAt: number;
-}
+export type Device = z.infer<typeof DeviceSchema>;
 
+export const TaskSchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  content: z.string(),
+  status: TaskStatusEnum,
+  createdAt: z.number(),
+  updatedAt: z.number(),
+});
+
+export type Task = z.infer<typeof TaskSchema>;
+
+export const ContextSchema = z.object({
+  id: z.string(),
+  index: z.number(),
+  content: z.string(),
+  createdAt: z.number(),
+  updatedAt: z.number(),
+});
+
+export type Context = z.infer<typeof ContextSchema>;
+
+// not need zod schema
 export interface Database {
   Devices: Device;
   Tasks: Task;
